4c31e65dd5d046ed4176554b9afc043c4662321c
jikou kin <jikou@fakemail.com>
2023-03-22 03:31:41 +0800

    add a few code

--- src/pages/skillpage.js
+++ src/pages/skillpage.js
@@ -1,7 +1,38 @@
 import React from "react";
+import htmlImg from "../img/html.png";
+import { useSpring, animated } from "react-spring";
+
+//react-spring usage 控制數字從１跑到 100
+function Number({ n }) {
+  const { number } = useSpring({
+    from: { number: 0 },
+    number: n,
+    delay: 200,
+    config: { mass: 1, tension: 20, friction: 10 },
+  });
+  return <animated.div>{number.to((n) => n.toFixed(0))}</animated.div>;
+}
 
 const skillpage = () => {
-  return <div>skillpage</div>;
+  return (
+    <div className="skill-bg">
+      <div className="skill-box">
+        <div className="skill-html">
+          <img src={htmlImg} alt="htmlIcon" />
+          <div className="html-circle-container">
+            <div className="circular-progress">
+              <span className="progress-value">
+                <Number n={85} />%
+              </span>
+            </div>
+          </div>
+          <div className="html-bar-container">
+            <div className=""></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default skillpage;
