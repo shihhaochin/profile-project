ca83ddbd52d56effd102ca0e81eb53eb0eb4e19b
jikou kin <jikou@fakemail.com>
2023-03-24 17:25:50 +0800

    add a few code

--- src/pages/skillpage.js
+++ src/pages/skillpage.js
@@ -1,5 +1,8 @@
 import React from "react";
 import htmlImg from "../img/html.png";
+import cssImg from "../img/css.png";
+import jsImg from "../img/jsImg.png";
+
 import { useSpring, animated } from "react-spring";
 
 //react-spring usage 控制數字從１跑到 100
@@ -20,14 +23,51 @@ const skillpage = () => {
         <div className="skill-html">
           <img src={htmlImg} alt="htmlIcon" />
           <div className="html-circle-container">
-            <div className="circular-progress">
-              <span className="progress-value">
+            <div className="html-circular-progress">
+              <span className="html-progress-value">
                 <Number n={85} />%
               </span>
             </div>
           </div>
           <div className="html-bar-container">
-            <div className=""></div>
+            <div className="html-bar"></div>
+            <span className="html-bar-value">
+              <Number n={85} />%
+            </span>
+          </div>
+        </div>
+
+        <div className="skill-css">
+          <img src={cssImg} alt="cssIcon" />
+          <div className="css-circle-container">
+            <div className="css-circular-progress">
+              <span className="css-progress-value">
+                <Number n={85} />%
+              </span>
+            </div>
+          </div>
+          <div className="css-bar-container">
+            <div className="css-bar"></div>
+            <span className="css-bar-value">
+              <Number n={85} />%
+            </span>
+          </div>
+        </div>
+
+        <div className="skill-js">
+          <img src={jsImg} alt="jsIcon" />
+          <div className="js-circle-container">
+            <div className="js-circular-progress">
+              <span className="js-progress-value">
+                <Number n={75} />%
+              </span>
+            </div>
+          </div>
+          <div className="js-bar-container">
+            <div className="js-bar"></div>
+            <span className="js-bar-value">
+              <Number n={75} />%
+            </span>
           </div>
         </div>
       </div>
