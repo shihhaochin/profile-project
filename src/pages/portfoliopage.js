18b60234bc4cbae54bcee65dd58ffe78ed710ea5
jikou kin <jikou@fakemail.com>
2023-03-25 16:05:02 +0800

    add a few code

--- src/pages/portfoliopage.js
+++ src/pages/portfoliopage.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const portfoliopage = () => {
-  return <div>portfoliopage</div>;
+  return (
+    <div className="portfolio-bg">
+      <div className="portfolio-box">
+        <div className="portfolio-card card-1  ">
+          <div className="portfolioImg01"></div>
+        </div>
+        <div className="portfolio-card card-2  ">
+          <div className="portfolioImg02"></div>
+        </div>
+        <div className="portfolio-card card-3  ">
+          <div className="portfolioImg03"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default portfoliopage;
