db0dfe7ee8a882324418aa633b29bfd0db9614c2
jikou kin <jikou@fakemail.com>
2023-03-29 02:01:41 +0800

    add a few code

--- src/pages/portfoliopage.js
+++ src/pages/portfoliopage.js
@@ -3,15 +3,33 @@ import React from "react";
 const portfoliopage = () => {
   return (
     <div className="portfolio-bg">
+      <div className="portfolio-title">
+        <h1>Portfolio</h1>
+        <h2>Latest projects</h2>
+        <span></span>
+      </div>
+
       <div className="portfolio-box">
         <div className="portfolio-card card-1  ">
-          <div className="portfolioImg01"></div>
+          <div className="portfolioImg">
+            <div className="portfolioImg01">
+              <span className="imgfilter01"></span>
+            </div>
+          </div>
         </div>
         <div className="portfolio-card card-2  ">
-          <div className="portfolioImg02"></div>
+          <div className="portfolioImg">
+            <div className="portfolioImg02">
+              <span className="imgfilter02"></span>
+            </div>
+          </div>
         </div>
         <div className="portfolio-card card-3  ">
-          <div className="portfolioImg03"></div>
+          <div className="portfolioImg">
+            <div className="portfolioImg03">
+              <span className="imgfilter03"></span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
