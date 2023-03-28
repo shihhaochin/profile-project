d1e42cd714d02b4d346e457f053ff590438eb52a
jikou kin <jikou@fakemail.com>
2023-03-28 22:17:09 +0800

    add a few code

--- src/pages/skillpage.js
+++ src/pages/skillpage.js
@@ -2,6 +2,9 @@ import React from "react";
 import htmlImg from "../img/html.png";
 import cssImg from "../img/css.png";
 import jsImg from "../img/jsImg.png";
+import reactImg from "../img/React.png";
+import nodejsImg from "../img/nodejs.png";
+import mongodbImg from "../img/Mongodb.png";
 
 import { useSpring, animated } from "react-spring";
 
@@ -19,55 +22,126 @@ function Number({ n }) {
 const skillpage = () => {
   return (
     <div className="skill-bg">
-      <div className="skill-box">
-        <div className="skill-html">
-          <img src={htmlImg} alt="htmlIcon" />
-          <div className="html-circle-container">
-            <div className="html-circular-progress">
-              <span className="html-progress-value">
-                <Number n={85} />%
-              </span>
-            </div>
-          </div>
-          <div className="html-bar-container">
-            <div className="html-bar"></div>
-            <span className="html-bar-value">
-              <Number n={85} />%
-            </span>
-          </div>
+      <h1>~What i know~</h1>
+      <div className="skill-content">
+        <div className="exprience-box">
+          <h1>My exprience</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, iure,
+            ut incidunt suscipit voluptates, officia molestias minima laborum
+            nostrum consequatur ipsa commodi. Incidunt quam voluptate
+            praesentium repellat obcaecati officia nemo! Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Officia maxime vero vel quae cum.
+            Quaerat hic ratione voluptatibus error? Officiis amet expedita
+            voluptatibus minima quam, ad iure itaque labore animi!
+          </p>
+          <button>Read more</button>
         </div>
 
-        <div className="skill-css">
-          <img src={cssImg} alt="cssIcon" />
-          <div className="css-circle-container">
-            <div className="css-circular-progress">
-              <span className="css-progress-value">
-                <Number n={85} />%
+        <div className="skill-box">
+          <h1>My skills</h1>
+          <h2>Fontend developer</h2>
+          <div className="skill-html">
+            <img src={htmlImg} alt="htmlIcon" />
+            <div className="html-circle-container">
+              <div className="html-circular-progress">
+                <span className="html-progress-value">
+                  <Number n={90} />%
+                </span>
+              </div>
+            </div>
+            <div className="html-bar-container">
+              <div className="html-bar"></div>
+              <span className="html-bar-value">
+                <Number n={90} />%
               </span>
             </div>
           </div>
-          <div className="css-bar-container">
-            <div className="css-bar"></div>
-            <span className="css-bar-value">
-              <Number n={85} />%
-            </span>
-          </div>
-        </div>
 
-        <div className="skill-js">
-          <img src={jsImg} alt="jsIcon" />
-          <div className="js-circle-container">
-            <div className="js-circular-progress">
-              <span className="js-progress-value">
-                <Number n={75} />%
+          <div className="skill-css">
+            <img src={cssImg} alt="cssIcon" />
+            <div className="css-circle-container">
+              <div className="css-circular-progress">
+                <span className="css-progress-value">
+                  <Number n={80} />%
+                </span>
+              </div>
+            </div>
+            <div className="css-bar-container">
+              <div className="css-bar"></div>
+              <span className="css-bar-value">
+                <Number n={80} />%
               </span>
             </div>
           </div>
-          <div className="js-bar-container">
-            <div className="js-bar"></div>
-            <span className="js-bar-value">
-              <Number n={75} />%
-            </span>
+
+          <div className="skill-js">
+            <img src={jsImg} alt="jsIcon" />
+            <div className="js-circle-container">
+              <div className="js-circular-progress">
+                <span className="js-progress-value">
+                  <Number n={70} />%
+                </span>
+              </div>
+            </div>
+            <div className="js-bar-container">
+              <div className="js-bar"></div>
+              <span className="js-bar-value">
+                <Number n={70} />%
+              </span>
+            </div>
+          </div>
+
+          <div className="skill-react">
+            <img src={reactImg} alt="reactIcon" />
+            <div className="react-circle-container">
+              <div className="react-circular-progress">
+                <span className="react-progress-value">
+                  <Number n={80} />%
+                </span>
+              </div>
+            </div>
+            <div className="react-bar-container">
+              <div className="react-bar"></div>
+              <span className="react-bar-value">
+                <Number n={80} />%
+              </span>
+            </div>
+          </div>
+          <h2>Backend developer</h2>
+
+          <div className="skill-node">
+            <img src={nodejsImg} alt="nodejs-Icon" />
+            <div className="node-circle-container">
+              <div className="node-circular-progress">
+                <span className="node-progress-value">
+                  <Number n={65} />%
+                </span>
+              </div>
+            </div>
+            <div className="node-bar-container">
+              <div className="node-bar"></div>
+              <span className="node-bar-value">
+                <Number n={65} />%
+              </span>
+            </div>
+          </div>
+
+          <div className="skill-mongo">
+            <img src={mongodbImg} alt="mongodb-Icon" />
+            <div className="mongo-circle-container">
+              <div className="mongo-circular-progress">
+                <span className="mongo-progress-value">
+                  <Number n={65} />%
+                </span>
+              </div>
+            </div>
+            <div className="mongo-bar-container">
+              <div className="mongo-bar"></div>
+              <span className="mongo-bar-value">
+                <Number n={65} />%
+              </span>
+            </div>
           </div>
         </div>
       </div>
